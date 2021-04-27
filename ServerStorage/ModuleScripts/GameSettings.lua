eccf726150d41e7db0363c600b0c3bf3324edb65
C-Hughes <Connor-Hughes@outlook.com>
2021-04-27 19:36:48 +0100

    Create GameSettings.lua

---
+++ ServerStorage/ModuleScripts/GameSettings.lua
@@ -0,0 +1,16 @@
+local GameSettings = {}
+
+-- Game Variables
+GameSettings.intermissionDuration = 2
+GameSettings.matchDuration = 20
+GameSettings.minimumPlayers = 2
+GameSettings.transitionTime = 5
+
+
+-- Possible ways that the game can end.
+GameSettings.endStates = {
+	TimerUp = "TimerUp",
+	FoundWinner = "FoundWinner"
+}
+
+return GameSettings
