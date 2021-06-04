77aa042693b6f09d06e12a39a6dea32b8e220095
C-Hughes <Connor-Hughes@outlook.com>
2021-06-05 00:31:23 +0100

    Update DisplayManager.lua

--- ServerStorage/ModuleScripts/DisplayManager.lua
+++ ServerStorage/ModuleScripts/DisplayManager.lua
@@ -2,29 +2,58 @@ local DisplayManager = {}
 
 -- Services
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ServerStorage = game:GetService("ServerStorage")
+
+-- Module Scripts
+local moduleScripts = ServerStorage:WaitForChild("ModuleScripts")
+local selectGameManager = require(moduleScripts:WaitForChild("SelectGameManager"))
+local playerManager = require(moduleScripts:WaitForChild("PlayerManager"))
 
 -- Display Values used to update Player GUI
 local displayValues = ReplicatedStorage:WaitForChild("DisplayValues")
 local status = displayValues:WaitForChild("Status")
 local playersLeft = displayValues:WaitForChild("PlayersLeft")
+local playerStatus = displayValues:WaitForChild("PlayerStatus")
 local timeLeft = displayValues:WaitForChild("TimeLeft")
 
 -- Local Functions
 local function updateRoundStatus()
-	status.Value = "Players Left: " .. playersLeft.Value .. " / Time Left: " .. timeLeft.Value
+	--status.Value = "Players Left: " .. playersLeft.Value .. " / Time Left: " .. timeLeft.Value
+	if selectGameManager.gameWinType == "LastManStanding" then
+		playerStatus.Value = "Players Remaining: " .. playersLeft.Value
+	else
+		playerStatus.Value = "Players Finished: " .. #playerManager.finishedPlayers .. "/" .. playerManager.maxPlayersThatCanFinish
+	end
 end
 
 -- Module Functions
+function DisplayManager.gameLoadingGUI(gameSelection)
+	--game.ReplicatedStorage.gameSelection:FireAllClients()
+	print("Displaying "..gameSelection.." Loading Screen........")
+	--game.ReplicatedStorage[gameSelection]:FireAllClients()
+	game.ReplicatedStorage.ToggleLoadingGUI:FireAllClients(gameSelection)
+end
+
+function DisplayManager.gameCutScene(gameSelection, timeToWait)
+	print("Displaying "..gameSelection.." CutScene........")
+	--local gameSelection = gameSelection .. "gameSelection"
+	--game.ReplicatedStorage[gameSelection.."CutScene"]:FireAllClients(timeToWait)
+	game.ReplicatedStorage.TriggerCutScene:FireAllClients(gameSelection, timeToWait)
+	
+end
+
 function DisplayManager.updateStatus(newStatus)
 	status.Value = newStatus
 end
 
+function DisplayManager.hideGameStatusGUI()
+	print("hideGameStatusGUI FIRE")
+	playerStatus.Value = " "
+	timeLeft.Value = 0
+end
+
 function DisplayManager.startCountdown()
 	
-	status.Value = "5"
-	wait(1)
-	status.Value = "4"
-	wait(1)
 	status.Value = "3"
 	wait(1)
 	status.Value = "2"
@@ -33,6 +62,7 @@ function DisplayManager.startCountdown()
 	wait(1)
 	status.Value = "GO!"
 	wait(1)
+	status.Value = " "
 	
 	--[[
 	status.Value = "3"
@@ -67,6 +97,6 @@ function DisplayManager.startCountdown()
 end
 
 playersLeft.Changed:Connect(updateRoundStatus)
-timeLeft.Changed:Connect(updateRoundStatus)
+--timeLeft.Changed:Connect(updateRoundStatus)
 
 return DisplayManager
